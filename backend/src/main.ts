01d702a04d17f123818f355c041617ebf0a0f591
Sibanye Bukani <sibanyebukani01@gmail.coom>
2022-08-01 22:42:33 +0200

    Dev Branch

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,7 +10,7 @@ import { TransformInterceptor } from './modules/common/interceptors/TransformInt
 import * as express from 'express';
 import { ErrorFilter } from './modules/errors/error.filter';
 
-console.log('Hello Sibanye')
+console.log('Hello Sibanye2')
 
 async function bootstrap() {
   const logger = new AppLogger();
